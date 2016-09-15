b64886bc4ed3f5e0beaa05e8b8663f3f98d5c1a5
cshg <csh@christianhaug.com>
2016-09-15 13:04:32 +0200

    Implement mapFilter chain to extract videos with specific boxart

---
+++ mapFilterChaining.js
@@ -0,0 +1,24 @@
+require('./concatAll');
+
+var movieLists = require('./movieLists');
+
+function getVideosWith150boxartUrls() {
+  return movieLists
+    .map(function(movieList) { return movieList.videos
+      .map(function(video) { return video.boxarts
+        .filter(function(boxart) { return boxart.width === 150; })
+        .map(function(boxart) {
+          return {
+            id: video.id,
+            title: video.title,
+            boxart: boxart.url
+          };
+        });
+      })
+      .concatAll()
+    })
+    .concatAll();
+}
+
+console.log(getVideosWith150boxartUrls());
+    
