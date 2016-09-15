799a4f1ae973a5f7ecf95cc311fdc5fc1213d57f
cshg <csh@christianhaug.com>
2016-09-15 13:49:00 +0200

    Refactor to ES6

--- mapFilterChaining.js
+++ mapFilterChaining.js
@@ -1,24 +1,36 @@
 require('./concatAll');
 
-var movieLists = require('./movieLists');
+const movieLists = require('./movieLists');
 
-function getVideosWith150boxartUrls() {
+function getVideosES6() {
   return movieLists
-    .map(function(movieList) { return movieList.videos
-      .map(function(video) { return video.boxarts
-        .filter(function(boxart) { return boxart.width === 150; })
-        .map(function(boxart) {
-          return {
-            id: video.id,
-            title: video.title,
-            boxart: boxart.url
-          };
-        });
-      })
+    .map(movieList => movieList.videos
+      .map(video => video.boxarts
+        .filter(boxart => boxart.width === 150)
+        .map(boxart => ({ id: video.id, title: video.title, boxart: boxart.url }))
+      )
       .concatAll()
-    })
+    )
     .concatAll();
 }
 
-console.log(getVideosWith150boxartUrls());
-    
+console.log(getVideosES6());
+
+// previous ES5 version for comparison
+function getVideosES5() {
+  return movieLists
+  .map(function(movieList) { return movieList.videos
+    .map(function(video) { return video.boxarts
+      .filter(function(boxart) { return boxart.width === 150; })
+      .map(function(boxart) {
+        return {
+          id: video.id,
+          title: video.title,
+          boxart: boxart.url,
+        };
+      });
+    })
+    .concatAll();
+  })
+  .concatAll();
+}
